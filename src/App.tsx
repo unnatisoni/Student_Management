c4e91c42e80380fe88301978db663a8fb74095a4
Your Name <you@example.com>
2021-07-15 02:57:53 +0530

    setup child routes for auth and dashboar pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route , Redirect, Switch } from 'react-router-dom';
+import AppContainerPage from './Page/AppContainer.page';
+import AuthHero from './components/AuthHero';
+import DashboardPage from './Page/Dashboard.page';
+import LoginPage from './Page/Login.page';
+import RecordingsPage from './Page/Recordings.page';
+import SignupPage from './Page/Signup.page';
+import AuthPage from './Page/Auth.page';
+import NotFoundPage from './Page/NotFound.page';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+       <Route path="/" exact>
+         <Redirect to= "./login"></Redirect>
+       </Route>
+       <Route path={["/Login", "/Signup"]} exact>
+         <AuthPage></AuthPage>
+       </Route>
+       <Route path={["/dashboard", "/recordings", "/batch/:batchNumber/lecture/:lectureNumber"]} exact>
+         <AppContainerPage></AppContainerPage>
+       </Route>
+        <Route>
+          <NotFoundPage />
+
+        </Route>
+      
+      </Switch>
+      </BrowserRouter>
+      
   );
 }
 
---
+++ src/Page/Login.page.tsx
@@ -0,0 +1,22 @@
+import {FC, memo} from "react";
+import { Link } from "react-router-dom";
+import AuthHero from "../components/AuthHero";
+interface Props{
+}
+const Login: React.FC<Props> = (props) => {
+  return (
+  
+        <div>
+        This is login page
+        Don't have a account <Link to="./Signup"><span className="text-blue-700">Signup page</span></Link>
+        <Link to="/dashboard"><span className="text-blue-700">Dashboard Page</span></Link>
+    </div>
+
+
+);
+};
+
+Login.defaultProps = {
+}
+
+export default memo(Login);
